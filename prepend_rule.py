7bae81b18bf1a9a5eb4cc9a47ff1467ee30d0094
Jamis Johnson <jamismanwaring@gmail.com>
2013-12-18 17:17:40 -0500

    add script to prepend rule to all gitignore files

---
+++ prepend_rule.py
@@ -0,0 +1,27 @@
+''' prepends a single rule to all gitignore files '''
+
+import sys, glob
+
+def prepend(line, filename):
+    # save contents of file
+    f = open(filename, 'r')
+    original = f.read()
+    f.close()
+    # reopen file, deleting all content
+    f = open(filename, 'w')
+    f.write(line)
+    # append original file contents
+    f.write(original)
+    f.close()
+
+if __name__ == '__main__':
+    rule = sys.argv[1]
+
+    # keep it simple, only allow adding 1 rule
+    if len(sys.argv) > 2:
+        print "warning: only adding 1 rule"
+    print "prepending rule: {0}".format(rule)
+
+    files = glob.glob('*.gitignore')
+    for filename in files:
+        prepend(rule, filename)
